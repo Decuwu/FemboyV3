9f43d71bde9353352ba227842b562eca31875c99
Decuwu <112495774+Decuwu@users.noreply.github.com>
2023-09-22 12:09:37 +0100

    Delete FemboyFiles/tables/blacklisted_repair_vehicles.lua

--- FemboyFiles/tables/blacklisted_repair_vehicles.lua
+++
@@ -1,9 +0,0 @@
-return {
-    ["Hydra"] = true,
-    ["F-160 Raiju"] = true,
-    ["Avenger"] = true,
-    ["Avenger2"] = true,
-    ["Avenger3"] = true,
-    ["Avenger4"] = true,
-    ["Tula"] = true
-}
